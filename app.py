7a2ef1ed97e9c51ac9537270cb1f0be91d35c98a
talkingdimwit <19037@wc.school.nz>
2023-04-16 00:32:41 +1200

    search and category system working

--- app.py
+++ app.py
@@ -24,16 +24,48 @@ def create_connection(db_file):
 def render_homepage():  # put application's code here
     return render_template('home.html')
 
-@app.route('/category')
-def render_category():  # put application's code here
+@app.route('/search', methods=['GET', 'POST'])
+def render_search():
+    search = request.form['search']
+    query = "SELECT maori, english, category, definition, level FROM Dictionary WHERE maori like ? OR english like ?  OR category like ? OR definition like ?  OR level like ?"
+    search = "%" + search + "%"
+    con = create_connection(DATABASE)
+    cur = con.cursor()
+    cur.execute(query, (search, search, search, search, search))
+    definition_list = cur.fetchall()
+    con.close()
+    print(definition_list)
+    return render_template('search.html', definintions=definition_list)
+
+@app.route('/words')
+def render_words():  # put application's code here
     con = create_connection(DATABASE)
     query = "SELECT maori, english, category, definition, level FROM Dictionary"
     cur = con.cursor()
     cur.execute(query)
     definition_list = cur.fetchall()
+    query = "SELECT id, category FROM categories"
+    cur = con.cursor()
+    cur.execute(query)
+    category_list = cur.fetchall()
     con.close()
     print(definition_list)
-    return render_template('category.html', definitions=definition_list)
+    return render_template('words.html', definitions=definition_list, categories=category_list)
+
+@app.route('/words/<category>')
+def render_words_category(category):  # put application's code here
+    con = create_connection(DATABASE)
+    query = "SELECT maori, english, category, definition, level FROM Dictionary WHERE category=?"
+    cur = con.cursor()
+    cur.execute(query, (category, ))
+    definition_list = cur.fetchall()
+    query = "SELECT id, category FROM categories"
+    cur = con.cursor()
+    cur.execute(query)
+    category_list = cur.fetchall()
+    con.close()
+    print(definition_list)
+    return render_template('words.html', definitions=definition_list, categories=category_list)
 
 @app.route('/login')
 def render_login():  # put application's code here
