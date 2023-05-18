b0621a77a40559bdf859aefca4c9d1a26e98c3bc
talkingdimwit <19037@wc.school.nz>
2023-05-19 03:58:55 +1200

    commets are done, added teacher code to sign up page for hand in

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def render_words():  # put application's code here
 @app.route('/words/<category>')
 def render_words_category(category):
     con = create_connection(DATABASE)
-    query = "SELECT maori, english, category, definition, level, editor FROM Dictionary WHERE category=?"
+    query = "SELECT maori, english, category, definition, level, editor FROM Dictionary WHERE category=?" #the WHERE category=? is makeing it so only whatever category is = to is getting grabed by the database
     cur = con.cursor()
     cur.execute(query, (category, ))
     definition_list = cur.fetchall()
@@ -129,13 +129,13 @@ def render_words_maori_admin(maori):  # put application's code here
     print(definition_list)
     return render_template('maori_admin.html', logged_in=is_logged_in(), definitions=definition_list, categories=category_list, is_teacher=is_teacher())
 
-#login page
+#page to login to account
 @app.route('/login', methods=['POST', 'GET'])
 def render_login():  # put application's code here
     if is_logged_in():
         return redirect('/')
     print("logging in")
-    if request.method == "POST":
+    if request.method == "POST": #POST means that we will be sending data to the database
         email = request.form['email'].strip().lower()
         password = request.form['password'].strip()
         print(email)
@@ -146,6 +146,7 @@ def render_login():  # put application's code here
         user_data = cur.fetchone()
         con.close
 
+        #checks that the email and password that were intered match with the one held in the database
         try:
             user_id = user_data[0]
             first_name = user_data[1]
@@ -155,9 +156,10 @@ def render_login():  # put application's code here
         except IndexError:
             return redirect("/login?error=Email+or+invalid+password+incorrect")
 
-        if not bcrypt.check_password_hash(db_password, password):
+        if not bcrypt.check_password_hash(db_password, password): #does a double check on the password checking if it matchs the incripted one held in the database
             return redirect(request.referrer, "?error=Email+or+invalid+password+inncorrect")
 
+        # this is storeing the user info server side and not in the database so we can easily grabed it as they are logged in
         session['email'] = email
         session['firstname'] = first_name
         session['lastname'] = last_name
@@ -168,6 +170,7 @@ def render_login():  # put application's code here
         return redirect('/')
     return render_template('login.html', is_teacher=is_teacher())
 
+#page to make account
 @app.route('/signup', methods=['POST', 'GET'])
 def render_signup_page():  # put application's code here
     if is_logged_in():
@@ -181,6 +184,7 @@ def render_signup_page():  # put application's code here
         password2 = request.form.get('password2')
         permission = request.form.get('code')
 
+        #checks if the user put in the correct teacher code if not user gets redirected to try again, if let blank user is a student
         if permission == '1111':
             permission = 'teacher'
         elif permission == '':
@@ -212,13 +216,16 @@ def render_signup_page():  # put application's code here
 
     return render_template('signup.html', logged_in=is_logged_in(), is_teacher=is_teacher())
 
+#page to logout
 @app.route('/logout')
 def logout():
+    #session.pop is releasing the info we had on the user server side, logging them out
     print(list(session.keys()))
     [session.pop(key) for key in list(session.keys())]
     print(list(session.keys()))
     return redirect('/?message=see+you+next+time!')
 
+#page to access admin controlles
 @app.route('/admin')
 def admin():
     if not is_logged_in():
@@ -231,12 +238,14 @@ def admin():
     con.close()
     return render_template("admin.html", logged_in=is_logged_in(), categories=category_list, is_teacher=is_teacher())
 
+#page that allows the user to add a word to the full dictionary
 @app.route("/add_word", methods=['POST'])
 def add_word():
     if not is_logged_in():
         redirect('/?message=need+to+be+logged+in')
     if request.method == "POST":
         print(request.form)
+        #request.form.get is grabing the inputs from the html pages given by the user
         word_maori = request.form.get('maori')
         word_english = request.form.get('english')
         word_category = request.form.get('category')
@@ -246,12 +255,15 @@ def add_word():
         word_editor = session.get("firstname")
         print(word_maori, word_english, word_category, word_definition, word_level,delete_word_confirm, word_editor)
         con = create_connection(DATABASE)
+        #inserts all varibles given by user into the dictionary makeing a word
         query = "INSERT INTO Dictionary (maori, english, category, definition, level, image, editor) VALUES (?, ?, ?, ?, ?, ?, ?)"
         cur = con.cursor()
         cur.execute(query, (word_maori, word_english, word_category, word_definition, word_level, word_image, word_editor))
         con.commit()
         con.close()
         return redirect("/admin")
+
+#page to delete words
 @app.route('/delete_word/<maori>', methods=['POST'])
 def delete_word(maori):
     if not is_logged_in():
@@ -259,7 +271,7 @@ def delete_word(maori):
     if request.method == "POST":
         maori = maori
         con = create_connection(DATABASE)
-        query = "SELECT id, maori FROM Dictionary WHERE maori=?"
+        query = "SELECT id, maori FROM Dictionary WHERE maori=?" #this is grabing the primary key for the word to prepare to delete it
         cur = con.cursor()
         cur.execute(query, (maori,))
         word = cur.fetchall()
@@ -267,12 +279,13 @@ def delete_word(maori):
         return render_template("delete_confirm.html", word=word)
     return redirect("/admin")
 
+#confrims with user they want to delete a word and then deletes it
 @app.route('/delete_word_confirm/<maori>')
 def delete_word_confirm(maori):
     if not is_logged_in():
         return redirect('/?message=need+to+be+logged+in')
     con = create_connection(DATABASE)
-    query = "DELETE FROM Dictionary WHERE id = ?"
+    query = "DELETE FROM Dictionary WHERE id = ?" #deletes the priamry key given by the last page only
     temp_word_id = maori[1]
     cur = con.cursor()
     cur.execute(query, (temp_word_id, ))
@@ -283,6 +296,7 @@ def delete_word_confirm(maori):
     print(temp_word_id)
     return redirect("/words")
 
+#lets user add new category (same as add word just for category)
 @app.route('/add_category', methods=['POST'])
 def add_category():
     if not is_logged_in():
@@ -299,6 +313,7 @@ def add_category():
         con.close()
         return redirect("/admin")
 
+#lets user delete a category (same as delete word just for category)
 @app.route('/delete_category', methods=['POST'])
 def delete_category():
     if not is_logged_in():
@@ -312,6 +327,7 @@ def delete_category():
         return render_template("delete_cat_confirm.html", id=cat_id, name=cat_name, type="category")
     return redirect("/admin")
 
+#confrims with user they want to delete a category and then deletes it (same as delete word confirm)
 @app.route('/delete_category_confirm/<id>')
 def delete_category_confirm(id):
     if not is_logged_in():
